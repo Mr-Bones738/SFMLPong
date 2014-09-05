b840c4eb415ccb107cd55a7fdece15083f6d3ddc
Mr-Bones738 <mr.bones738@gmail.com>
2014-09-05 12:16:18 +0100

    Pointless setposition

--- SFMLPong/Source/Paddle.cpp
+++ SFMLPong/Source/Paddle.cpp
@@ -6,7 +6,6 @@ bool Paddle::init(sf::RenderWindow *window){
 	}
 	tx_paddle.setSmooth(false);
 	sp_paddle.setTexture(tx_paddle);
-	sp_paddle.setPosition(10, window->getSize().y / 2);
 	sp_paddle.setOrigin(8, 64);
 	return true;
 }
